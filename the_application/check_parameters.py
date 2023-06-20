573cdd1eb31f0c3389e3107b546c03ef73b3cfe8
Simonavet <simona.hassanein@gmail.com>
2023-06-20 21:00:10 +0300

    first commit

---
+++ the_application/check_parameters.py
@@ -0,0 +1,166 @@
+class Horse:
+
+    def horse_respiration(self, hr):
+        if hr < 12:
+            print('The respiratory rate is low')
+        elif 12 <= hr <= 20:
+            print('The respiratory rate is normal')
+        else:
+            print('The respiratory rate is high')
+
+    def horse_heart_beat(self, hhb):
+        if hhb < 38:
+            print('The heart beat rate is low')
+        elif 38 <= hhb <= 44:
+            print('The heart beat rate is normal')
+        else:
+            print('The heart beat rate is high')
+
+    def horse_temperature(self, ht):
+        if ht < 37:
+            print('The temperature is low')
+        elif 37 <= ht <= 38.5:
+            print('The temperature is normal')
+        else:
+            print('The temperature is high')
+
+class Bovine:
+
+    def bovine_respiration(self, br):
+        if br < 30:
+            print('The respiratory rate is low')
+        elif br == 30:
+            print('The respiratory rate is normal')
+        else:
+            print('The respiratory rate is high')
+
+    def bovine_heart_beat(self, bhb):
+        if bhb < 60:
+            print('The heart beat rate is low')
+        elif 60 <= bhb <= 70:
+            print('The heart beat rate is normal')
+        else:
+            print('The heart beat rate is high')
+
+    def bovine_temperature(self, bt):
+        if bt < 38.3:
+            print('The temperature is low')
+        elif 38.3 <= bt <= 38.6:
+            print('The temperature is normal')
+        else:
+            print('The temperature is high')
+
+class Dog:
+
+    def dog_respiration(self, dr):
+        if dr < 22:
+            print('The respiratory rate is low')
+        elif dr == 22:
+            print('The respiratory rate is normal')
+        else:
+            print('The respiratory rate is high')
+
+    def dog_heart_beat(self, dhb):
+        if dhb < 100:
+            print('The heart beat rate is low')
+        elif 100 <= dhb <= 130:
+            print('The heart beat rate is normal')
+        else:
+            print('The heart beat rate is high')
+
+    def dog_temperature(self, dt):
+        if dt < 38.9:
+            print('The temperature is low')
+        elif dt == 38.9:
+            print('The temperature is normal')
+        else:
+            print('The temperature is high')
+
+
+class Cat:
+
+    def cat_respiration(self, cr):
+        if cr < 26:
+            print('The respiratory rate is low')
+        elif cr == 26:
+            print('The respiratory rate is normal')
+        else:
+            print('The respiratory rate is high')
+
+    def cat_heart_beat(self, chb):
+        if chb < 110:
+            print('The heart beat rate is low')
+        elif 110 <= chb <= 140:
+            print('The heart beat rate is normal')
+        else:
+            print('The heart beat rate is high')
+
+    def cat_temperature(self, ct):
+        if ct < 38.6:
+            print('The temperature is low')
+        elif ct == 38.6:
+            print('The temperature is normal')
+        else:
+            print('The temperature is high')
+
+
+class Guinea_pig:
+
+    def guinea_pig_respiration(self, gpr):
+        if gpr < 40:
+            print('The respiratory rate is low')
+        elif 40 <= gpr <= 100:
+            print('The respiratory rate is normal')
+        else:
+            print('The respiratory rate is high')
+
+    def guinea_pig_heart_beat(self, gphb):
+        if gphb < 230:
+            print('The heart beat rate is low')
+        elif 230 <= gphb <= 380:
+            print('The heart beat rate is normal')
+        else:
+            print('The heart beat rate is high')
+
+    def guinea_pig_temperature(self, gpt):
+        if gpt < 37.2:
+            print('The temperature is low')
+        elif 37.2 <= gpt <= 38.6:
+            print('The temperature is normal')
+        else:
+            print('The temperature is high')
+
+
+class Lion:
+
+    def lion_respiration(self, lr):
+        if lr < 20:
+            print('The respiratory rate is low')
+        elif lr == 20:
+            print('The respiratory rate is normal')
+        else:
+            print('The respiratory rate is high')
+
+    def lion_heart_beat(self, lhb):
+        if lhb < 40:
+            print('The heart beat rate is low')
+        elif lhb == 40:
+            print('The heart beat rate is normal')
+        else:
+            print('The heart beat rate is high')
+
+    def lion_temperature(self, lt):
+        if lt < 38.05:
+            print('The temperature is low')
+        elif lt <= 39.2:
+            print('The temperature is normal')
+        else:
+            print('The temperature is high')
+
+
+
+
+
+
+
+
---
+++ the_application/working_functions.py
@@ -0,0 +1,48 @@
+from the_application.check_parameters import *
+
+horse = Horse()
+cow = Bovine()
+dog = Dog()
+cat = Cat()
+guinea_pig = Guinea_pig()
+lion = Lion()
+
+print('Name specie')
+x = str(input())
+
+if x == 'horse':
+    print(f'Respiratory rate: '), horse.horse_respiration(hr=int(input()))
+    print(f'Heart beat: '), horse.horse_heart_beat(hhb=int(input()))
+    print(f'Temperature: '), horse.horse_temperature(ht=float(input()))
+
+elif x == 'cow':
+    print(f'Respiratory rate: '), cow.bovine_respiration(br=int(input()))
+    print(f'Heart beat: '), cow.bovine_heart_beat(bhb=int(input()))
+    print(f'Temperature: '), cow.bovine_temperature(bt=float(input()))
+
+elif x == 'dog':
+    print(f'Respiratory rate: '), dog.dog_respiration(dr=int(input()))
+    print(f'Heart beat: '), dog.dog_heart_beat(dhb=int(input()))
+    print(f'Temperature: '), dog.dog_temperature(dt=float(input()))
+
+elif x == 'cat':
+    print(f'Respiratory rate: '), cat.cat_respiration(cr=int(input()))
+    print(f'Heart beat: '), cat.cat_heart_beat(chb=int(input()))
+    print(f'Temperature: '), cat.cat_temperature(ct=float(input()))
+
+
+elif x == 'guinea pig':
+    print(f'Respiratory rate: '), guinea_pig.guinea_pig_respiration(gpr=int(input()))
+    print(f'Heart beat: '), guinea_pig.guinea_pig_heart_beat(gphb=int(input()))
+    print(f'Temperature: '), guinea_pig.guinea_pig_temperature(gpt=float(input()))
+
+elif x == 'lion':
+    print(f'Respiratory rate: '), lion.lion_respiration(lr=int(input()))
+    print(f'Heart beat: '), lion.lion_heart_beat(lhb=int(input()))
+    print(f'Temperature: '), lion.lion_temperature(lt=float(input()))
+
+
+else:
+    print('I do not know this specie')
+
+
